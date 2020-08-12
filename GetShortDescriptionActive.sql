15e9f187368633e16ae9f7f4022e43241da94cc9
angerangel <angerangel@gmail.com>
2020-08-12 12:06:24 +0200

    Create GetShortDescriptionActive.sql

---
+++ GetShortDescriptionActive.sql
@@ -0,0 +1,23 @@
+#This get all short description of active products
+
+Select   
+SQL_CALC_FOUND_ROWS 
+ps8n_product_lang.id_product ,
+ps8n_product_lang.name ,
+ps8n_product_lang.id_lang ,
+ps8n_product_lang.description_short  
+      FROM 
+	  ps8n_product_lang
+	  INNER JOIN
+	 ( ps8n_product,  ps8n_product_shop)
+	  ON 
+	  ( 
+	  ps8n_product_lang.id_product =    ps8n_product.id_product
+	  AND 
+	  ps8n_product_lang.id_product =    ps8n_product_shop.id_product
+	  )	  
+WHERE  
+
+ps8n_product_shop.active = 1
+ 
+ORDER BY  ps8n_product_lang.name ;  
