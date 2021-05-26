5ffa241d71b165fc3db91777cca344189f72e777
angerangel <angerangel@gmail.com>
2021-05-26 09:07:37 +0200

    Create UpdateAddAttachmentForProducts.sql

---
+++ UpdateAddAttachmentForProducts.sql
@@ -0,0 +1,5 @@
+# the table containing the files is ps8n_attachment
+# there you'll find id_attachment of the file and the file_name.
+# table ps8n_attachment_lang contains for which languages is the attachment, with name and description (so every language has the desription in language)
+# table ps8n_product_attachment  link id_product with id_attachment
+  
