d79700acae459792498ca75c15913b09cfb5f50f
angerangel <angerangel@gmail.com>
2021-06-03 10:31:42 +0200

    Update UpdateAddAttachmentForProducts.sql

--- UpdateAddAttachmentForProducts.sql
+++ UpdateAddAttachmentForProducts.sql
@@ -6,4 +6,7 @@
 
 SELECT DISTINCT `id_product` , `name` FROM `ps8n_product_lang` #this help you to search id_product, add a WHERE clause to filter the result
 
+#This return all products without attachemnt
+SELECT DISTINCT `id_product` , `name` FROM `ps8n_product_lang` where not exists (Select DISTINCT id_product FROM ps8n_product_attachment WHERE id_product = ps8n_product_lang.id_product ) ; 
+
 INSERT INTO ps8n_product_attachment (id_product , id_attachment ) VALUES( 1, 1 ) ON DUPLICATE KEY UPDATE   #change 1 and 1 with the values you need
