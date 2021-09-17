d61fe8933492bb4da90d8586742c4ecb439b15be
angerangel <angerangel@gmail.com>
2021-09-17 20:06:33 +0200

    Update get_Item_on_sale.sql

--- get_Item_on_sale.sql
+++ get_Item_on_sale.sql
@@ -7,6 +7,6 @@ SELECT ps8n_product_lang.name,  ps8n_product_lang.description_short
       AND 
       ps8n_product_shop.active=true 
       AND
-      ps8n_product_land.id_lang=1;
+      ps8n_product_lang.id_lang=1;
       
       --This way we gat all in English
