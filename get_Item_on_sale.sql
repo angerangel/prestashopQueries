766254cf67643891e394c059a1d2ae6160fe8894
angerangel <angerangel@gmail.com>
2021-09-17 20:05:57 +0200

    Update get_Item_on_sale.sql

--- get_Item_on_sale.sql
+++ get_Item_on_sale.sql
@@ -1,6 +1,12 @@
 SELECT ps8n_product_lang.name,  ps8n_product_lang.description_short   
-   FROM ps8n_product,ps8n_product_lang  
+   FROM ps8n_product,ps8n_product_lang,   ps8n_product_shop
    WHERE 
-      ps8n_product.id_product =  ps8n_product_lang.id_product  
+      ps8n_product.id_product =  ps8n_product_lang.id_product 
+      AND
+      ps8n_product.id_product =  ps8n_product_shop.id_product 
       AND 
-      ps8n_product.on_sale=true ;
+      ps8n_product_shop.active=true 
+      AND
+      ps8n_product_land.id_lang=1;
+      
+      --This way we gat all in English
