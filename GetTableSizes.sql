4280ee03f3d15801c2e380051747c7ea0cb9bdd0
angerangel <angerangel@gmail.com>
2021-09-17 22:12:01 +0200

    Create GetTableSizes.sql

---
+++ GetTableSizes.sql
@@ -0,0 +1,8 @@
+SELECT 
+     table_schema as `Database`, 
+     table_name AS `Table`, 
+     round(((data_length + index_length) / 1024 / 1024), 2) `Size in MB` 
+FROM information_schema.TABLES 
+ORDER BY (data_length + index_length) DESC;
+
+-- Prestashop DB sometime grows too much, so here we can see what is fat!
