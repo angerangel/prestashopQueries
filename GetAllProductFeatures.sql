4faa7034b9b8c689ff597883b453293e045b6714
angerangel <angerangel@gmail.com>
2021-09-17 21:20:56 +0200

    Create GetAllProductFeatures.sql

---
+++ GetAllProductFeatures.sql
@@ -0,0 +1,18 @@
+
+SELECT 
+ps8n_product.id_product, ps8n_product_lang.name, ps8n_product_lang.description_short ,  
+ps8n_feature_lang.name, ps8n_feature_value_lang.value
+FROM 
+ps8n_product, ps8n_product_lang  ,  ps8n_product_shop, ps8n_feature_lang, ps8n_feature_value_lang,  ps8n_feature_product
+WHERE 
+ps8n_product.id_product =  ps8n_product_lang.id_product 
+AND ps8n_product.id_product=ps8n_product_shop.id_product
+AND  ps8n_product_shop.active=true 
+AND ps8n_product_lang.id_lang=2
+AND  ps8n_feature_lang.id_lang=2
+AND ps8n_feature_value_lang.id_lang=2
+AND  ps8n_product.id_product=ps8n_feature_product.id_product
+AND ps8n_feature_product.id_feature = ps8n_feature_lang.id_feature
+AND ps8n_feature_value_lang.id_feature_value = ps8n_feature_product.id_feature_value
+order BY ps8n_product_lang.name, ps8n_feature_lang.name
+;
