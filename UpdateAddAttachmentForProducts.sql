3d623c7339a3d3952d45d05308b7baf2fe38fa06
angerangel <angerangel@gmail.com>
2022-02-24 10:02:00 +0100

    Update UpdateAddAttachmentForProducts.sql

--- UpdateAddAttachmentForProducts.sql
+++ UpdateAddAttachmentForProducts.sql
@@ -4,7 +4,9 @@
 # table ps8n_product_attachment  link id_product with id_attachment
 # table ps8n_product_lang contain product name and id_product
 
-SELECT DISTINCT `id_product` , `name` FROM `ps8n_product_lang` #this help you to search id_product, add a WHERE clause to filter the result
+SELECT DISTINCT `id_product` , `name` FROM `ps8n_product_lang` ; #this help you to search id_product, add a WHERE clause to filter the result
+
+SELECT id_attachment FROM ps8n_product_attachment WHERE id_product = 23 ;  #this way you find the id attachment of a product (change 23 with id of your product)
 
 #This return all products without attachemnt and active
 SELECT DISTINCT  id_product , name 
@@ -29,4 +31,4 @@ ps8n_product_lang.id_product = ps8n_product_shop.id_product
 ;
 
 
-INSERT INTO ps8n_product_attachment (id_product , id_attachment ) VALUES( 1, 1 ) ON DUPLICATE KEY UPDATE   #change 1 and 1 with the values you need
+INSERT INTO ps8n_product_attachment (id_product , id_attachment ) VALUES( 1, 1 ) ON DUPLICATE KEY UPDATE  ; #change 1 and 1 with the values you need
