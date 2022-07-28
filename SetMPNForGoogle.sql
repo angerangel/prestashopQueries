2c9e564fb02cd4948a5d4ed885d8cb14f35462ac
angerangel <angerangel@gmail.com>
2022-07-28 18:55:05 +0200

    Create SetMPNForGoogle.sql

---
+++ SetMPNForGoogle.sql
@@ -0,0 +1,3 @@
+#If your products ha not an MPN, this create a valid MPN
+
+UPDATE  ps8n_product, ps8n_product_lang  SET ps8n_product.mpn=ps8n_product_lang.name WHERE  ps8n_product.id_product=ps8n_product_lang.id_product   ;
