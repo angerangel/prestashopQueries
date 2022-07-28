9bff721c404dcf14c5527e92483267b5fd0af234
angerangel <angerangel@gmail.com>
2022-07-28 22:30:25 +0200

    Create DownloadMetaTitles.sql

---
+++ DownloadMetaTitles.sql
@@ -0,0 +1,3 @@
+#Download Meta Titles
+
+select  name, meta_title FROM ps8n_product_lang, ps8n_product_shop  WHERE  ps8n_product_lang.id_product   =  ps8n_product_shop.id_product  AND ps8n_product_shop.active=true    INTO OUTFILE "/var/lib/mysql-files/ElencoMetaTitle2.txt" ;
