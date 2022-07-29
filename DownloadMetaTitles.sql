8db49749b1e3bde9d412e15058d335e8decfef9b
angerangel <angerangel@gmail.com>
2022-07-29 13:33:12 +0200

    Update DownloadMetaTitles.sql

--- DownloadMetaTitles.sql
+++ DownloadMetaTitles.sql
@@ -5,4 +5,4 @@ select  name, meta_title FROM ps8n_product_lang, ps8n_product_shop
      ps8n_product_lang.id_product   =  ps8n_product_shop.id_product  
      AND
      ps8n_product_shop.active=true
-     INTO OUTFILE "/var/lib/mysql-files/ElencoMetaTitle2.txt" ;
+     INTO OUTFILE "/var/lib/mysql-files/ElencoMetaTitle2.csv" ;
