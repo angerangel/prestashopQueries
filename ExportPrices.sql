38072032232349570a023045030110315f166960
angerangel <angerangel@gmail.com>
2022-07-29 18:38:55 +0200

    Update ExportPrices.sql

--- ExportPrices.sql
+++ ExportPrices.sql
@@ -2,27 +2,38 @@
 #This way you can create a price list to print for users, ordered by category and by code.
 #I have to use a variable for creating the query, because the SELECT statement doesn't permits function in file name.
 #The § char is very common on keyboards, but nobody use it,so it's very handful to separate filed and avoi problems with carriage returns.
+#It show the last 2 parent categories (Example: Parts/Wheels)
 
 SET @sql = CONCAT(
-"SELECT ps8n_category_lang.name, ps8n_product_lang.name, ps8n_product_lang.description_short,  ps8n_product.price  FROM  ps8n_product_lang, ps8n_product,  ps8n_product_shop, ps8n_category_lang
-    WHERE 
-    ps8n_product.id_product=ps8n_product_lang.id_product
+    "SELECT
+        ps8n_category_lang.name, t1.cname, t1.pname, t1.ds,t1.price
+     FROM
+     ps8n_category_lang,  ps8n_category,
+    (SELECT  ps8n_product.id_category_default as 'id_c', ps8n_category_lang.name as 'cname', ps8n_product_lang.name as 'pname', ps8n_product_lang.description_short as 'ds',  ps8n_product.price as 'price' 
+         FROM  ps8n_product_lang, ps8n_product,  ps8n_product_shop, ps8n_category_lang 
+        WHERE 
+        ps8n_product.id_product=ps8n_product_lang.id_product
+        AND
+        ps8n_product.id_category_default=ps8n_category_lang.id_category
+        AND
+        ps8n_category_lang.id_lang=2
+        AND
+        ps8n_product_lang.id_lang=2
+        AND 
+        ps8n_product_shop.active=true 
+        AND
+        ps8n_product.id_product=ps8n_product_shop.id_product  ) as t1
+    WHERE
+        t1.id_c=ps8n_category.id_category
     AND
-    ps8n_product.id_category_default=ps8n_category_lang.id_category
+        ps8n_category_lang.id_category=ps8n_category.id_parent
     AND
-    ps8n_category_lang.id_lang=2
-    AND
-    ps8n_product_lang.id_lang=2
-    AND 
-    ps8n_product_shop.active=true 
-    AND
-    ps8n_product.id_product=ps8n_product_shop.id_product
-    ORDER BY ps8n_category_lang.name, ps8n_product_lang.name  
+        ps8n_category_lang.id_lang=2 
+    ORDER BY ps8n_category_lang.name, t1.cname, t1.pname
     INTO OUTFILE '",
-     CONCAT('/var/lib/mysql-files/Listino-',  NOW() + 0, '.csv\' ') ,  
-    " FIELDS ENCLOSED BY '§' ;" )  ;
+    CONCAT('/var/lib/mysql-files/Listino-',  NOW() + 0, '.csv\' ') ,  " FIELDS ENCLOSED BY '§' ;" 
+    );
 
     PREPARE statement FROM @sql;
-
     EXECUTE statement;
     DEALLOCATE PREPARE statement;
