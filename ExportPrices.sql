7ccd18982580e7da37ced658e034e59a5ec935d8
angerangel <angerangel@gmail.com>
2022-08-26 14:18:35 +0200

    Update ExportPrices.sql

--- ExportPrices.sql
+++ ExportPrices.sql
@@ -5,15 +5,16 @@
 #It show the last 2 parent categories (Example: Parts/Wheels)
 #Please note that MySql permits to write only in a specific folder, in my case (ububtu) /var/lib/mysql-files .
 
-
 SET @sql = CONCAT(
     "SELECT
         ps8n_category_lang.name, t1.cname, t1.pname, t1.ds,t1.price
      FROM
      ps8n_category_lang,  ps8n_category,
-    (SELECT  ps8n_product.id_category_default as 'id_c', ps8n_category_lang.name as 'cname', ps8n_product_lang.name as 'pname', replace(ps8n_product_lang.description_short,'\n','' ) as 'ds',  ps8n_product.price as 'price' 
-         FROM  ps8n_product_lang, ps8n_product,  ps8n_product_shop, ps8n_category_lang 
-        WHERE 
+    (SELECT  ps8n_product.id_category_default as 'id_c', ps8n_category_lang.name as 'cname', ps8n_product_lang.name as 'pname',
+    replace(replace(replace(replace(ps8n_product_lang.description_short,'\n','' ),'<p>',' '),'</p>',' '),'<br>',' ') as 'ds',
+    ps8n_product.price as 'price'
+         FROM  ps8n_product_lang, ps8n_product,  ps8n_product_shop, ps8n_category_lang
+        WHERE
         ps8n_product.id_product=ps8n_product_lang.id_product
         AND
         ps8n_product.id_category_default=ps8n_category_lang.id_category
@@ -21,8 +22,8 @@ SET @sql = CONCAT(
         ps8n_category_lang.id_lang=2
         AND
         ps8n_product_lang.id_lang=2
-        AND 
-        ps8n_product_shop.active=true 
+        AND
+        ps8n_product_shop.active=true
         AND
         ps8n_product.id_product=ps8n_product_shop.id_product  ) as t1
     WHERE
@@ -30,7 +31,7 @@ SET @sql = CONCAT(
     AND
         ps8n_category_lang.id_category=ps8n_category.id_parent
     AND
-        ps8n_category_lang.id_lang=2 
+        ps8n_category_lang.id_lang=2
     ORDER BY ps8n_category_lang.name, t1.cname, t1.pname
     INTO OUTFILE '",
     CONCAT('/var/lib/mysql-files/Listino-',  NOW() + 0, '.csv\' ') ,  " FIELDS ENCLOSED BY '§' ;"
@@ -38,4 +39,4 @@ SET @sql = CONCAT(
 
     PREPARE statement FROM @sql;
     EXECUTE statement;
-    DEALLOCATE PREPARE statement;
+
