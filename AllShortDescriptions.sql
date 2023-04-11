0d62c9c4fc61b51613ba52ad55497ebf51ffb825
angerangel <angerangel@gmail.com>
2023-04-11 15:09:02 +0200

    Update AllShortDescriptions.sql

--- AllShortDescriptions.sql
+++ AllShortDescriptions.sql
@@ -1,12 +1,11 @@
-##Select all active product with short description of a category
+#This code show all the short description of the active product of a category, and of a signle language of you choiche.
 
-SELECT ps8n_product_lang.name,  ps8n_product_lang.description_short   
-   FROM ps8n_product, ps8n_product_lang,   ps8n_product_shop
-   WHERE 
-      ps8n_product.id_product =  ps8n_product_lang.id_product 
-      AND 
-      ps8n_product_shop.active=true 
-      AND
-      ps8n_product_lang.id_lang=2 #for the second language
-      AND
-      ps8n_product.id_category_default = 993 ; #for the category
+SELECT DISTINCT ps8n_product_lang.name,  ps8n_product_lang.description_short   
+FROM ps8n_product, ps8n_product_lang,   ps8n_product_shop   
+WHERE      
+ps8n_product.id_product =  ps8n_product_lang.id_product     
+AND        ps8n_product_shop.active=true     
+AND       ps8n_product_lang.id_lang=1  #language
+AND       ps8n_product.id_category_default = 993  #category
+ORDER BY ps8n_product_lang.name    ;
+
