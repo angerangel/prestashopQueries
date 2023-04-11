414fd872c93222d0023791e66b7ffb9a98a1a657
angerangel <angerangel@gmail.com>
2023-04-11 11:39:24 +0200

    Create AllShortDescriptions.sql

---
+++ AllShortDescriptions.sql
@@ -0,0 +1,10 @@
+SELECT ps8n_product_lang.name,  ps8n_product_lang.description_short   
+   FROM ps8n_product,ps8n_product_lang,   ps8n_product_shop
+   WHERE 
+      ps8n_product.id_product =  ps8n_product_lang.id_product 
+      AND
+      ps8n_product.id_product =  ps8n_product_shop.id_product 
+      AND 
+      ps8n_product_shop.active=true 
+      AND
+      ps8n_product_lang.id_lang=2; #for the second language
