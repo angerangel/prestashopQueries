370e0b8ac3c79b34b82e673f0002f69ae845143a
angerangel <angerangel@gmail.com>
2023-04-28 14:54:35 +0200

    Create stockValue.sql

---
+++ stockValue.sql
@@ -0,0 +1,22 @@
+#basic version
+
+SELECT SUM(`quantity` * `price`) FROM ps8n_product
+
+#only active products
+
+SELECT SUM(`quantity` * `price`) FROM ps8n_product WHERE active=true ;
+
+#Only for one Brand,
+#TIP: to get brand numbers just use:  
+#     select * from ps8n_manufacturer  ;
+
+SELECT  ps8n_product_lang.name, ps8n_product.quantity  , ps8n_product.price  
+FROM  ps8n_product_lang, ps8n_product, ps8n_product_shop
+WHERE
+ps8n_product.id_product=ps8n_product_lang.id_product
+AND
+ps8n_product.id_product=ps8n_product_shop.id_product
+AND
+ps8n_product.id_manufacturer=4 
+AND 
+ps8n_product_shop.active=true ;
