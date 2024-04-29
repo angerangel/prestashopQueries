593bf7247238792eb3fdb2ea3a3feb0c31d3ba12
angerangel <angerangel@gmail.com>
2024-04-29 14:47:20 +0200

    Update SQL_Manager.sql

--- SQL_Manager.sql
+++ SQL_Manager.sql
@@ -1,5 +1,6 @@
+-- this method is to insert queries in backoffice SQL Manager
 -- the table containing the all SQL queries in Prestashop SQL Manager is ps8n_request_sql 
--- please note the the name of column sql is a reserved word, so you have to tu backstick char to use it, like `sql`
+-- please note the the name of column sql is a reserved word, so you have to escape with backstick char to use it, like `sql`
 
 INSERT into ps8n_request_sql (`name`,`sql`) VALUES ('my query','SELECT * FROM ps8n_request_sql') ;
 
