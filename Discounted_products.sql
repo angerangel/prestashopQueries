ad3c9cefca890f933cab841e854773fb237f6585
angerangel <angerangel@gmail.com>
2025-12-24 15:58:14 +0100

    Create Discounted_products.sql

---
+++ Discounted_products.sql
@@ -0,0 +1,14 @@
+--Get the discounted product active
+
+SELECT ps8n_product_lang.name ,  ps8n_product.id_product   
+   FROM ps8n_product,ps8n_product_lang,   ps8n_product_shop
+   WHERE 
+      ps8n_product.id_product =  ps8n_product_lang.id_product 
+      AND
+      ps8n_product.id_product =  ps8n_product_shop.id_product 
+      AND 
+      ps8n_product_shop.active=true 
+      AND
+	  ps8n_product_shop.on_sale = 1
+	  AND
+      ps8n_product_lang.id_lang=1;
